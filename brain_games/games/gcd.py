c7d8222cac1c90e01583cc8069c578c421fd46ca
potemkuh@gmail.com <potemkuh@gmail.com>
2021-02-09 13:50:46 +0300

    fix gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -3,27 +3,36 @@ from math import gcd
 import prompt
 
 
-def game_over():
-    print(
-        f"'{answer}' is wrong answer ;(. Correct answer was '{gcd(number_a,number_b)}'."
-    )
+def game_over(answer, name, result):
+    print(f"'{answer}' is wrong answer ;(.")
+    print(f"Correct answer was '{result}'.")
     print(f"Let's try again, {name}!")
+
+
+def get_numbers():
+    number_a = randint(1, 100)
+    number_b = randint(1, 100)
+    return number_a, number_b
+
+
+def answer(arg_1, arg_2):
+    if arg_1 == arg_2:
+        return True
+    return False
 
 
 def game_nod(name):
     print('Find the greatest common divisor of given numbers.')
     game_round = 0
-    while True:
-        number_a = randint(1, 100)
-        number_b = randint(1, 100)
-        game_round += 1
-        print(f'Question: {number_a} {number_b}')
-        answer = prompt.string('Your answer: ')
-        if game_round == 3:
-            print(f'Congratulations, {name}!')
-            return
-        elif answer == str(gcd(number_a, number_b)):
+    while game_round < 3:
+        num_1, num_2 = get_numbers()
+        result = gcd(num_1, num_2)
+        print(result)
+        print(f'Question: {num_1} {num_2}')
+        user_answer = prompt.string('Your answer: ')
+        if answer(int(user_answer), result) == True:
+            game_round += 1
             print('Correct!')
         else:
-            game_over()
-            return
+            return game_over(user_answer, name, result)
+    print(f'Congratulations, {name}!')
