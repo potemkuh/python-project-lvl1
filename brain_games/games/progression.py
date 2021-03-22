14e9aa8bc948cdb853a960cca5adf05759c888ca
potemkuh@gmail.com <potemkuh@gmail.com>
2021-03-22 20:11:49 +0300

    bugfix

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -6,7 +6,7 @@ def question_and_answer():
     #size = random.randint(5, 10)
     start = random.randint(1, 100)
     step = random.randint(2, 10)
-    question = [start + step * i for i in range(10)]
+    question = [str(start + step * i) for i in range(10)]
     index = random.randint(0, len(question) - 1)
     result = question[index]
     question[index] = "..."
